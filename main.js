d780ea0841d44a57bdee405482df0acbbeb63e1c
Mario Vazquez <mariov@unprg.edu.pe>
2022-10-24 13:56:23 -0500

    create index html and script js

---
+++ main.js
@@ -0,0 +1,102 @@
+const navEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
+navEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleMenuProductDetail);
+
+function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleMenuProductDetail() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "TV",
+  price: 700,
+  img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Mobile",
+  price: 1200,
+  img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+
+function renderProducts(array){
+    for (product of array) {
+        const productCard = document.createElement("div"); // creamos el div
+        productCard.classList.add("product-card");  // agregamos clase
+        
+        //product = {name, price, img} -> produc.img
+      
+        const productImage = document.createElement("img");
+        productImage.setAttribute("src", product.img); // modificamos propiedad y agregamos la ruta 
+      
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info"); 
+      
+        const productInfoDiv = document.createElement("div"); // div sin clase 
+      
+        const productPrice = document.createElement("p"); // innerText, innerHtml, inner , append
+        productPrice.innerText = "$" + product.price; // parrafo precio
+      
+        const productName = document.createElement("p"); // // parrafo name 
+        productName.innerText = product.name;
+      
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+      
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+      
+        productInfoFigure.appendChild(productImgCart); //insertamos a su elemento padre
+      
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+      
+        productCard.appendChild(productImage);
+        productCard.appendChild(productInfo);
+      
+        cardsContainer.appendChild(productCard);
+      }
+}
+
+renderProducts(productList);
+
+//Con Element.append() podemos agregar varios nodos y texto mientras que con Element.appendChild() solo podemos agregar un nodo.
+// productInfoDiv.append(productPrice, productName)
